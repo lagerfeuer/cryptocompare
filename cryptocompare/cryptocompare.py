75c31f14a88b0788924c37b60db2b1b1ee3a1ba2
Fede Caccia <federico.caccia@coinfabrik.com>
2019-06-06 17:07:29 -0300

    adding limit parameter in historical get requests

--- cryptocompare/cryptocompare.py
+++ cryptocompare/cryptocompare.py
@@ -8,8 +8,8 @@ URL_PRICE = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms={}&tsyms={}
 URL_PRICE_MULTI = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms={}&tsyms={}'
 URL_PRICE_MULTI_FULL = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms={}&tsyms={}'
 URL_HIST_PRICE = 'https://min-api.cryptocompare.com/data/pricehistorical?fsym={}&tsyms={}&ts={}&e={}'
-URL_HIST_PRICE_DAY = 'https://min-api.cryptocompare.com/data/histoday?fsym={}&tsym={}'
-URL_HIST_PRICE_HOUR = 'https://min-api.cryptocompare.com/data/histohour?fsym={}&tsym={}'
+URL_HIST_PRICE_DAY = 'https://min-api.cryptocompare.com/data/histoday?fsym={}&tsym={}&limit={}'
+URL_HIST_PRICE_HOUR = 'https://min-api.cryptocompare.com/data/histohour?fsym={}&tsym={}&limit={}'
 URL_HIST_PRICE_MINUTE = 'https://min-api.cryptocompare.com/data/histominute?fsym={}&tsym={}&limit={}'
 URL_AVG = 'https://min-api.cryptocompare.com/data/generateAvg?fsym={}&tsym={}&e={}'
 URL_EXCHANGES = 'https://www.cryptocompare.com/api/data/exchanges'
@@ -71,11 +71,11 @@ def get_historical_price(coin, curr=CURR, timestamp=time.time(), exchange='CCCAG
     return query_cryptocompare(URL_HIST_PRICE.format(coin, format_parameter(curr),
         int(timestamp), format_parameter(exchange)))
 
-def get_historical_price_day(coin, curr=CURR):
-    return query_cryptocompare(URL_HIST_PRICE_DAY.format(coin, format_parameter(curr)))
+def get_historical_price_day(coin, curr=CURR, limit=LIMIT):
+    return query_cryptocompare(URL_HIST_PRICE_DAY.format(coin, format_parameter(curr), limit))
 
-def get_historical_price_hour(coin, curr=CURR):
-    return query_cryptocompare(URL_HIST_PRICE_HOUR.format(coin, format_parameter(curr)))
+def get_historical_price_hour(coin, curr=CURR, limit=LIMIT):
+    return query_cryptocompare(URL_HIST_PRICE_HOUR.format(coin, format_parameter(curr), limit))
 
 def get_historical_price_minute(coin, curr=CURR, limit=LIMIT):
     return query_cryptocompare(URL_HIST_PRICE_MINUTE.format(coin, format_parameter(curr), limit))
