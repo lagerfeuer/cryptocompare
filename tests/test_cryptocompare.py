1ad83c2aff2a8ec61c1dd0b0a3e49525707e79a4
Lukas Deutz <lukas.deutz@mailfence.com>
2021-01-25 08:28:32 -0500

    Fix failing tests

--- tests/test_cryptocompare.py
+++ tests/test_cryptocompare.py
@@ -6,93 +6,97 @@ import datetime
 
 
 class TestCryptoCompare(unittest.TestCase):
-  def assertCoinAndCurrInPrice(self, coin, curr, price):
-    if isinstance(coin, list):
-      for co in coin:
-        self.assertCoinAndCurrInPrice(co, curr, price)
-      return
-    else:
-      self.assertIn(coin, price)
+    def assertCoinAndCurrInPrice(self, coin, curr, price):
+        if isinstance(coin, list):
+            for co in coin:
+                self.assertCoinAndCurrInPrice(co, curr, price)
+            return
+        else:
+            self.assertIn(coin, price)
 
-    if isinstance(curr, list):
-      for cu in curr:
-        self.assertIn(cu, price[coin])
-    else:
-      self.assertIn(curr, price[coin])
+        if isinstance(curr, list):
+            for cu in curr:
+                self.assertIn(cu, price[coin])
+        else:
+            self.assertIn(curr, price[coin])
 
-  def test_coin_list(self):
-    lst = cryptocompare.get_coin_list()
-    self.assertTrue('BTC' in lst.keys())
-    lst = cryptocompare.get_coin_list(True)
-    self.assertTrue('BTC' in lst)
+    def test_coin_list(self):
+        lst = cryptocompare.get_coin_list()
+        self.assertTrue('BTC' in lst.keys())
+        lst = cryptocompare.get_coin_list(True)
+        self.assertTrue('BTC' in lst)
 
-  def test_get_price(self):
-    coin = 'BTC'
-    price = cryptocompare.get_price(coin)
-    self.assertCoinAndCurrInPrice(coin, 'EUR', price)
-    price = cryptocompare.get_price(coin, curr='USD')
-    self.assertCoinAndCurrInPrice(coin, 'USD', price)
-    currencies = ['EUR', 'USD', 'GBP']
-    price = cryptocompare.get_price(coin, curr=currencies)
-    self.assertCoinAndCurrInPrice(coin, currencies, price)
-    coins = ['BTC', 'XMR']
-    price = cryptocompare.get_price(coins, curr=currencies)
-    self.assertCoinAndCurrInPrice(coins, currencies, price)
+    def test_get_price(self):
+        coin = 'BTC'
+        price = cryptocompare.get_price(coin)
+        self.assertCoinAndCurrInPrice(coin, 'EUR', price)
+        price = cryptocompare.get_price(coin, currency='USD')
+        self.assertCoinAndCurrInPrice(coin, 'USD', price)
+        currencies = ['EUR', 'USD', 'GBP']
+        price = cryptocompare.get_price(coin, currency=currencies)
+        self.assertCoinAndCurrInPrice(coin, currencies, price)
+        coins = ['BTC', 'XMR']
+        price = cryptocompare.get_price(coins, currency=currencies)
+        self.assertCoinAndCurrInPrice(coins, currencies, price)
 
-  def test_get_price_full(self):
-    price = cryptocompare.get_price('ETH', full=True)
-    self.assertIn('RAW', price)
-    self.assertIn('ETH', price['RAW'])
-    self.assertIn('EUR', price['RAW']['ETH'])
-    self.assertIn('PRICE', price['RAW']['ETH']['EUR'])
+    def test_get_price_full(self):
+        price = cryptocompare.get_price('ETH', full=True)
+        self.assertIn('RAW', price)
+        self.assertIn('ETH', price['RAW'])
+        self.assertIn('EUR', price['RAW']['ETH'])
+        self.assertIn('PRICE', price['RAW']['ETH']['EUR'])
 
-  def test_get_historical_price(self):
-    coin = 'XMR'
-    curr = 'EUR'
-    price = cryptocompare.get_historical_price(
-        'XMR', timestamp=datetime.date(2017, 6, 6))
-    self.assertCoinAndCurrInPrice(coin, curr, price)
-    price2 = cryptocompare.get_historical_price(
-        'XMR', 'EUR', datetime.datetime(2017, 6, 6))
-    self.assertCoinAndCurrInPrice(coin, curr, price2)
-    self.assertEqual(price, price2)
+    def test_get_historical_price(self):
+        coin = 'XMR'
+        curr = 'EUR'
+        price = cryptocompare.get_historical_price(
+            'XMR', timestamp=datetime.date(2017, 6, 6))
+        self.assertCoinAndCurrInPrice(coin, curr, price)
+        price2 = cryptocompare.get_historical_price(
+            'XMR', 'EUR', datetime.datetime(2017, 6, 6))
+        self.assertCoinAndCurrInPrice(coin, curr, price2)
+        self.assertEqual(price, price2)
 
-  def test_price_day(self):
-    coin = 'BTC'
-    curr = 'USD'
-    price = cryptocompare.get_historical_price_day(coin, curr=curr, limit=3, exchange='CCCAGG', toTs=datetime.datetime(2019,6,6))
-    for frame in price:
-      self.assertIn('time', frame)
+    def test_price_day(self):
+        coin = 'BTC'
+        curr = 'USD'
+        price = cryptocompare.get_historical_price_day(
+            coin, currency=curr, limit=3, exchange='CCCAGG', toTs=datetime.datetime(2019, 6, 6))
+        for frame in price:
+            self.assertIn('time', frame)
 
-  def test_price_hour(self):
-    coin = 'BTC'
-    curr = 'USD'
-    price = cryptocompare.get_historical_price_hour(coin, curr=curr, limit=3, exchange='CCCAGG', toTs=datetime.datetime(2019,6,6,12))
-    for frame in price:
-      self.assertIn('time', frame)
+    def test_price_hour(self):
+        coin = 'BTC'
+        curr = 'USD'
+        price = cryptocompare.get_historical_price_hour(
+            coin, currency=curr, limit=3, exchange='CCCAGG', toTs=datetime.datetime(2019, 6, 6, 12))
+        for frame in price:
+            self.assertIn('time', frame)
 
-  def test_price_minute(self):
-    coin = 'BTC'
-    curr = 'USD'
-    price = cryptocompare.get_historical_price_minute(coin, curr=curr, limit=3, exchange='CCCAGG', toTs=datetime.datetime.now())
-    for frame in price:
-      self.assertIn('time', frame)
+    def test_price_minute(self):
+        coin = 'BTC'
+        curr = 'USD'
+        price = cryptocompare.get_historical_price_minute(
+            coin, currency=curr, limit=3, exchange='CCCAGG', toTs=datetime.datetime.now())
+        for frame in price:
+            self.assertIn('time', frame)
 
-  def test_get_avg(self):
-    coin = 'BTC'
-    curr = 'USD'
-    avg = cryptocompare.get_avg(coin, curr, exchange='Kraken')
-    self.assertEqual(avg['LASTMARKET'], 'Kraken')
-    self.assertEqual(avg['FROMSYMBOL'], coin)
-    self.assertEqual(avg['TOSYMBOL'], curr)
+    def test_get_avg(self):
+        coin = 'BTC'
+        curr = 'USD'
+        avg = cryptocompare.get_avg(coin, curr, exchange='Kraken')
+        self.assertEqual(avg['LASTMARKET'], 'Kraken')
+        self.assertEqual(avg['FROMSYMBOL'], coin)
+        self.assertEqual(avg['TOSYMBOL'], curr)
 
-  def test_get_exchanges(self):
-    exchanges = cryptocompare.get_exchanges()
-    self.assertIn('Kraken', exchanges)
+    def test_get_exchanges(self):
+        exchanges = cryptocompare.get_exchanges()
+        self.assertIn('Kraken', exchanges)
 
-  def test_get_pairs(self):
-    pairs = cryptocompare.get_pairs(exchange='Kraken')
-    self.assertEqual('Kraken', pairs[0]['exchange'])
+    def test_get_pairs(self):
+        pairs = cryptocompare.get_pairs(exchange='Kraken')
+        self.assertEqual('Kraken', pairs[0]['exchange'])
+
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
