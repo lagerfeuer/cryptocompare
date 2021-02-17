9cb13fd76424ea12130ea06ead2abab26c9b64ad
Lukas Deutz <lukas.deutz@mailfence.com>
2021-02-17 13:23:44 -0500

    Bump version to 0.7.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding="utf-8") as f:
 
 setup(
     name='cryptocompare',
-    version='0.7.4',
+    version='0.7.5',
     description='Wrapper for CryptoCompare.com',
     long_description=readme,
     long_description_content_type='text/markdown',
